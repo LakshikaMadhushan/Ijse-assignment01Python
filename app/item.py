fcb639331f74715bcf1ab8f9b1289a11e05578ad
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2021-11-07 13:19:57 +0530

    initialize db init

--- app/item.py
+++ app/item.py
@@ -7,3 +7,15 @@ __db_location__ = "db"
 __session_file__  = f"{__db_location__}/session.db"
 __item_folder__  =  f"{__db_location__}/item"
 __item_last_id__  =  f"{__db_location__}/item_id.db"
+
+def init(arguments):
+
+    def db():
+        os.makedirs(__item_folder__)
+
+    section = arguments[0]
+    if section == "init":
+        command = arguments[1]
+        params = arguments[2:]
+        if command == "db":
+            db()
