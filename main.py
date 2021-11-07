44b74b201da1a68021d7bc4f553ef1f5a1b96e07
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2021-11-07 13:24:54 +0530

    create database

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import sys 
+import sys
+import json
+import os
+from pprint import pprint
+
+from app.item import init
+from app.item import login
+
+if __name__=="__main__":
+    arguments = sys.argv[1:]
+    init(arguments)
+    section = arguments[0]
+    command = arguments[1]
+    params = arguments[2:]
+
+    if section == "user":
+         if command == "login":
+             login(*params )
